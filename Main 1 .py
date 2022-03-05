6b73bf13ae0eeb538eb7bd11befc007ef98de6aa
MatteoBenin <100286514+MatteBex@users.noreply.github.com>
2022-03-05 15:54:06 +0100

    Add files via upload

---
+++ Main 1 .py
@@ -0,0 +1,11 @@
+countOrange=0
+countBlue=0
+while True:
+    if countOrange==12 and countBlue==12:
+        # Stop() 
+        print("Stop")
+        break
+    print("Vai")
+    countOrange+=1
+    countBlue+=1
+input("Premi un tasto per continuare")
